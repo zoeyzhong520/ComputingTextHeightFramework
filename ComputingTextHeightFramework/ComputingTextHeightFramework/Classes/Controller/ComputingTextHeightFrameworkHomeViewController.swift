9ce545766f4009de6895e7e0d97050a32d8ec718
ZHONG ZHAOJUN <joe@zhongdemacbook-pro.local>
2018-04-13 21:52:25 +0800

    - fix bug

--- ComputingTextHeightFramework/ComputingTextHeightFramework/Classes/Controller/ComputingTextHeightFrameworkHomeViewController.swift
+++ ComputingTextHeightFramework/ComputingTextHeightFramework/Classes/Controller/ComputingTextHeightFrameworkHomeViewController.swift
@@ -16,6 +16,15 @@ class ComputingTextHeightFrameworkHomeViewController: ComputingTextHeightFramewo
     ///单行Label
     var singleLineLabel:UILabel!
     
+    ///UIScrollView
+    var scrollView:UIScrollView!
+    
+    ///TestLabelH
+    var TestLabelH:CGFloat = 0.0
+    
+    ///SingleLineLabelH
+    var SingleLineLabelH:CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setPage()
@@ -34,13 +43,29 @@ extension ComputingTextHeightFrameworkHomeViewController {
     fileprivate func setPage() {
         
         self.title = "Home"
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.addScrollView()
         self.addTestLabel()
         self.addSingleLineLabel()
+        self.setContentSizeOfScrollView()
+    }
+    
+    fileprivate func addScrollView() {
+        
+        self.scrollView = UIScrollView(frame: CGRect(x: 0, y: NavigationBarHeight + StatusBarHeight, width: screenWidth, height: screenHeight - (NavigationBarHeight + StatusBarHeight)))
+        scrollView.backgroundColor = UIColor.cyan
+        self.view.addSubview(scrollView)
+    }
+    
+    ///contentSize of scrollView
+    fileprivate func setContentSizeOfScrollView() {
+    
+        self.scrollView.contentSize = CGSize(width: screenWidth, height: self.TestLabelH + self.SingleLineLabelH + 20*3)
     }
     
     fileprivate func addTestLabel() {
         
-        self.testLabel = UILabel(frame: CGRect(x: 20, y: 100, width: screenWidth - 20*2, height: 100))
+        self.testLabel = UILabel()
         testLabel.backgroundColor = UIColor.lightGray
         testLabel.text = TextForLabel
         testLabel.numberOfLines = 0
@@ -48,23 +73,23 @@ extension ComputingTextHeightFrameworkHomeViewController {
         testLabel.textAlignment = .left
         
         testLabel.setText(text: TextForLabel, lineSpaing: 8, labelWidth: screenWidth - 20*2, font: UIFont.systemFont(ofSize: 14)) //设置行间距
-        let labelHeight = TextForLabel.heightWithLabelFont(font: UIFont.systemFont(ofSize: 14), labelWidth: screenWidth - 20*2, lineSpacing: 8, label: testLabel)
-        testLabel.frame = CGRect(x: 20, y: 100, width: screenWidth - 20*2, height: labelHeight)
-        self.view.addSubview(testLabel)
+        self.TestLabelH = TextForLabel.heightWithLabelFont(font: UIFont.systemFont(ofSize: 14), labelWidth: screenWidth - 20*2, lineSpacing: 8, label: testLabel)
+        testLabel.frame = CGRect(x: 20, y: 20, width: screenWidth - 20*2, height: self.TestLabelH)
+        self.scrollView.addSubview(testLabel)
     }
     
     fileprivate func addSingleLineLabel() {
         
-        self.singleLineLabel = UILabel(frame: CGRect(x: 20, y: self.testLabel.frame.maxY + 20, width: screenWidth - 20*2, height: 100))
+        self.singleLineLabel = UILabel()
         singleLineLabel.backgroundColor = UIColor.lightGray
         singleLineLabel.text = "这是单行文本。"
         singleLineLabel.numberOfLines = 0
         singleLineLabel.font = UIFont.systemFont(ofSize: 14)
         singleLineLabel.textAlignment = .left
         
-        let labelHeight = singleLineLabel.text!.heightWithLabelFont(font: UIFont.systemFont(ofSize: 14))
-        singleLineLabel.frame = CGRect(x: 20, y: self.testLabel.frame.maxY + 20, width: screenWidth - 20*2, height: labelHeight)
-        self.view.addSubview(singleLineLabel)
+        self.SingleLineLabelH = singleLineLabel.text!.heightWithLabelFont(font: UIFont.systemFont(ofSize: 14))
+        singleLineLabel.frame = CGRect(x: 20, y: self.testLabel.frame.maxY + 20, width: screenWidth - 20*2, height: self.SingleLineLabelH)
+        self.scrollView.addSubview(singleLineLabel)
     }
 }
 
